f84f0da99d886279e9bf9fad293d5767ac0d3dce
Gillian Riches <martingillian4@gmail.com>
2021-11-18 15:02:18 -0700

    Added random_clusters target that is made from the randomClusters() function. Not yet working

--- _targets.R
+++ _targets.R
@@ -9,8 +9,11 @@ tar_option_set(packages = c("dplyr","hms", "lubridate", "tidyverse", "leaflet", 
 # End this file with a list of target objects.
 list(
   tar_target(caps, makeCaps("data")),
-  tar_target(clusters_per_date,caps_tr(caps)),
+  tar_target(clusters_per_date,caps_tr(caps, params)),
   tar_target(raw_data_maps, makeRawDataMaps(clusters_per_date)),
-  tar_target(cluster_maps, makeClusterMaps(clusters_per_date))
+  tar_target(cluster_maps, makeClusterMaps(clusters_per_date)),
+  # This is where I set the ranges of parameters I want to take random samples from
+  tar_target(random_clusters, randomClusters(eps = c(1:40), minpts = c(1:10), delta_t = c(300:1200), 
+                                           entr_t = c(0.5:2.50), ndraws = 5))
 )
 
