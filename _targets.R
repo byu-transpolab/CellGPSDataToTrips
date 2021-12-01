c807cdd91a77fc35835e2d85563f4c2c7a7ef8b6
Gillian Riches <martingillian4@gmail.com>
2021-11-30 22:04:56 -0700

    Got random_clusters target working. Had to change MinPts to be minpts

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ tar_option_set(packages = c("dplyr","hms", "lubridate", "tidyverse", "leaflet", 
 # End this file with a list of target objects.
 list(
   tar_target(caps, makeCaps("data/SensorData-1617304845160.zip")),
-  tar_target(random_clusters, randomClusters(caps = caps[2:5,], eps = c(1:40), minpts = c(1:10), delta_t = c(300:1200), ndraws = 5)),
+  tar_target(random_clusters, randomClusters(caps = caps[2:5,], eps = c(1:40), minPts = c(1:10), delta_t = c(300:1200), ndraws = 5)),
   tar_target(params, random_clusters$params),
   tar_target(raw_data_maps, makeRawDataMaps(caps, random_clusters))
   #tar_target(cluster_maps, makeClusterMaps(clusters_per_date)),
