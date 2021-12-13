1d738af9825c45ac445c7b975844c79b8f61132e
Gillian Riches <martingillian4@gmail.com>
2021-12-13 15:35:06 -0700

    Added manualTable and matchStats targets where matchStats is the final comparison table

--- _targets.R
+++ _targets.R
@@ -2,18 +2,18 @@ library(targets)
 
 source("R/gps2trips.R")
 source("R/MakeMaps.R")
+source("R/MyAnalysis.R")
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("dplyr","hms", "lubridate", "tidyverse", "leaflet", "sf", "gpsactivs", "ggspatial"))
+tar_option_set(packages = c("dplyr","hms", "lubridate", "tidyverse", "leaflet", "sf", "gpsactivs", "ggspatial", "data.table"))
 
 # End this file with a list of target objects.
 list(
-  tar_target(caps, makeCaps("data/SensorData-1617304845160.zip")),
-  tar_target(random_clusters, randomClusters(caps = caps[2:5,], eps = c(1:40), minPts = c(1:10), delta_t = c(300:1200), ndraws = 5)),
+  tar_target(caps, makeCaps("data/SensorData-1596231685391.zip")),
+  tar_target(random_clusters, randomClusters(caps[1:3,], eps = 40, minPts = 20, delta_t = 400, ndraws = 5)),
   tar_target(params, random_clusters$params),
-  tar_target(raw_data_maps, makeRawDataMaps(caps, random_clusters))
-  #tar_target(cluster_maps, makeClusterMaps(clusters_per_date)),
-  # This is where I set the ranges of parameters I want to take random samples from
-  # tar_target(random_clusters, randomClusters(caps = caps[2:5,], eps = c(1:40), minPts = c(1:10), delta_t = c(300:1200), ndraws = 5))
+  # tar_target(raw_data_maps, makeRawDataMaps(caps = caps, random_clusters)),
+  tar_target(manualTable, getGeoJson(folder = "manual_clusters")),
+  tar_target(matchStats, calculateMatchStats(random_clusters, manualTable))
 )
 
