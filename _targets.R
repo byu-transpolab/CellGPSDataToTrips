48622c0cd1f07cd234f235ea1f8cd3ac9463dff9
gillianriches <martingillian4@gmail.com>
2022-02-18 13:48:59 -0700

    add the sumErrors target

--- _targets.R
+++ _targets.R
@@ -4,15 +4,16 @@ source("R/gps2trips.R")
 source("R/MyAnalysis.R")
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("dplyr","hms", "lubridate", "tidyverse", "leaflet", "sf", "gpsactivs", "ggspatial", "data.table", "plotly"))
+tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", "tidyverse", "leaflet", "sf", "gpsactivs", "ggspatial", "data.table", "plotly"))
 
 # End this file with a list of target objects.
 list(
   tar_target(caps, makeCaps("data")),
-  tar_target(random_clusters, randomClusters(caps, eps = 50, minPts = 50, delta_t = 400, entr_t = 1.0, ndraws = 100)),
+  tar_target(random_clusters, randomClusters(caps[1:4], eps = 50, minPts = 50, delta_t = 400, entr_t = 1.0, ndraws = 3)),
   tar_target(params, random_clusters$params),
   tar_target(manualTable, getGeoJson(folder = "manual_clusters")),
   tar_target(matchStats, calculateMatchStats(random_clusters, manualTable)),
-  tar_target(errorPlot, pctErrorPlot(matchStats))
+  tar_target(errorPlot, pctErrorPlot(matchStats)),
+  tar_target(sumErrors, sumError(matchStats))
 )
 
