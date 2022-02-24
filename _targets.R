a86a2ebea1984ec1f0c9a35247adc084c5338b93
gillianriches <martingillian4@gmail.com>
2022-02-24 16:22:25 -0700

    add the algorithm_table target that comes from the joinTables function

--- _targets.R
+++ _targets.R
@@ -1,7 +1,8 @@
 library(targets)
 
-source("R/gps2trips.R")
-source("R/MyAnalysis.R")
+#source("R/gps2trips.R")
+#source("R/MyAnalysis.R")
+source("Optimize.R")
 
 # Set target-specific options such as packages.
 tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", 
@@ -10,10 +11,11 @@ tar_option_set(packages = c("dplyr","tools", "hms", "lubridate",
 
 # End this file with a list of target objects.
 list(
-  tar_target(cleaned_data, makeCaps("data")),
-  tar_target(manual_table, getGeoJson("manual_clusters")),
-  tar_target(alg_manual_table, joinTables(manual_table, cleaned_data)),
+  tar_target(cleaned_data, cleanData("data")),
+  tar_target(algorithm_table, makeAlgorithmTable(params = c(1,2,3,4), cleaned_data[2,])),
+  tar_target(manual_table, makeManualTable("manual_clusters")),
+  tar_target(alg_manual_table, joinTables(manual_table, algorithm_table)),
   tar_target(optimized_params, optimize(alg_manual_table)),
-  tar_target(num_clusters, make_clusters(optimized_params, alg_manual_table))
+  tar_target(num_clusters, makeClusters(optimized_params, alg_manual_table))
 )
 
