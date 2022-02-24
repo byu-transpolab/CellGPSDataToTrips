bd0fd0ec564c5ac70092525a7aef2c07edfb8a90
gillianriches <martingillian4@gmail.com>
2022-02-24 14:40:08 -0700

    Starting from new targets file using the optimization method instead of random draws method

--- _targets.R
+++ _targets.R
@@ -4,16 +4,16 @@ source("R/gps2trips.R")
 source("R/MyAnalysis.R")
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", "tidyverse", "leaflet", "sf", "gpsactivs", "ggspatial", "data.table", "plotly"))
+tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", 
+                            "tidyverse", "leaflet", "sf", "gpsactivs", 
+                            "ggspatial", "data.table", "plotly"))
 
 # End this file with a list of target objects.
 list(
-  tar_target(caps, makeCaps("data")),
-  tar_target(random_clusters, randomClusters(caps[1:4], eps = 50, minPts = 50, delta_t = 400, entr_t = 1.0, ndraws = 3)),
-  tar_target(params, random_clusters$params),
-  tar_target(manualTable, getGeoJson(folder = "manual_clusters")),
-  tar_target(matchStats, calculateMatchStats(random_clusters, manualTable)),
-  tar_target(errorPlot, pctErrorPlot(matchStats)),
-  tar_target(sumErrors, sumError(matchStats))
+  tar_target(cleaned_data, makeCaps("data")),
+  tar_target(manual_table, getGeoJson("manual_clusters")),
+  tar_target(alg_manual_table, joinTables(manual_table, cleaned_data)),
+  tar_target(optimized_params, optimize(alg_manual_table)),
+  tar_target(num_clusters, make_clusters(optimized_params, alg_manual_table))
 )
 
