15631b5d1787d690ac22afd62b1c30361a6f31d8
gillianriches <martingillian4@gmail.com>
2022-03-16 14:43:15 -0600

    Add the optimx library and viridis libraries. Viridis library is necessary for the "maps" target and optimx may be necessary for the "optimizedparams" target depending on what optimization function we use. Add the "maps" target which is the output of the MakeMaps.R script.

--- _targets.R
+++ _targets.R
@@ -1,18 +1,22 @@
 library(targets)
 
 source("R/Optimize.R")
+source("R/MakeMaps.R")
 
 # Set target-specific options such as packages.
 tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", 
                             "tidyverse", "leaflet", "sf", "gpsactivs", 
-                            "ggspatial", "data.table", "plotly"))
+                            "ggspatial", "data.table", "plotly", "optimx",
+                            "viridis"))
 
 # End this file with a list of target objects.
 list(
   tar_target(cleaned_data, cleanData("data")),
+  tar_target(maps, makeAllMaps(cleaned_data)),
   tar_target(manual_table, makeManualTable("manual_clusters")),
   tar_target(cleaned_manual_table, joinTables(manual_table, cleaned_data)),
   tar_target(optimized_params, optimize(cleaned_manual_table)),
-  tar_target(num_clusters, makeClusters(params = optimized_params$par, cleaned_manual_table))
+  tar_target(num_clusters, makeClusters(cleaned_manual_table = cleaned_data,
+                                        params = optimized_params$par))
 )
 
