e0accb83c4705fd1f3b70c68ac33ecceb8c85e17
gillianriches <martingillian4@gmail.com>
2022-03-16 15:03:21 -0600

    Add information about each function including the parameter, what is returned, and details if necessary

--- R/MakeMaps.R
+++ R/MakeMaps.R
@@ -1,3 +1,12 @@
+#' Function to make maps of the cleaned GPS data
+#'
+#'
+#' @param cleaned_data target
+#' @return geom_sf map of raw GPS data
+#' @details the GPS points are colored by time to help
+#' visualize when activities were made and/or if multiple
+#' activities were made to the same place at different times
+
 makeMaps <- function(cleaned_data,id, mydate) {
   st_as_sf((cleaned_data %>% filter(id == id, date == mydate))$data[[1]], 
            coords = c("lon", "lat"), crs = 4326) %>%
@@ -7,6 +16,15 @@ makeMaps <- function(cleaned_data,id, mydate) {
     ) + scale_color_viridis()
 }
 
+#' Function to get randon IDs and Dates to map
+#'
+#'
+#' @param cleaned_data target
+#' @return tibble of 10 random IDs with 3 dates each
+#' @details the returned tibble is the IDs and dates that will
+#' be mapped in order to get random days and numbers of 
+#' clusters
+
 getRandomDates <- function(cleaned_data) {
   myIDs <- unique(cleaned_data$id)
   randomID <- sample(myIDs, size = min(10,length(myIDs)))
@@ -14,6 +32,14 @@ getRandomDates <- function(cleaned_data) {
     group_by(id) %>% slice_sample(n = 3)
   return(randomDate)
 }
+
+#' Function to clean GPS data
+#'
+#'
+#' @param cleaned_data target
+#' @return list of maps, specifically the "maps" target
+#' @details makes a map for each randpm day for each 
+#' of the 10 random IDs
 
 makeAllMaps <- function(cleaned_data){
   myDays <- getRandomDates(cleaned_data)
