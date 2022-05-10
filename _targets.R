6af6fc035bdeb9bc3a6eff0d277552ae0db194e1
gillianriches <martingillian4@gmail.com>
2022-05-09 20:51:14 -0600

    Add accurate_tibble target which is output of using optimized params. Add num_trips table, activity_types table, and final giant table which isnt made yet

--- _targets.R
+++ _targets.R
@@ -7,20 +7,24 @@ library(furrr)
 source("R/Optimize.R")
 source("R/MakeMaps.R")
 
+tar_option_set(debug = "optimized_params")
+
 # Set target-specific options such as packages.
 tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", 
                             "tidyverse", "leaflet", "sf", "gpsactivs", 
                             "ggspatial", "data.table", "plotly", 
                             "viridis", "pomp"))
-
 plan(multisession)
 # End this file with a list of target objects.
 list(
   tar_target(cleaned_data, cleanData("data")),
-  tar_target(maps, makeAllMaps(cleaned_data)),
+  #tar_target(maps, makeAllMaps(cleaned_data)),
   tar_target(manual_table, makeManualTable("manual_clusters")),
   tar_target(cleaned_manual_table, joinTables(manual_table, cleaned_data)),
   tar_target(optimized_params, optimize(cleaned_manual_table)),
-  tar_target(num_clusters, makeClusters(cleaned_manual_table = cleaned_data,
-                                        params = optimized_params$par))
+  tar_target(accurate_tibble, makeClusters(cleaned_manual_table = cleaned_data,
+                                        params = optimized_params$par)),
+  tar_target(num_trips, addNumtrips(accurate_tibble)),
+  tar_target(activity_types, addTtripType(num_trips)),
+  tar_target(final_table, addMentalHealthResponses(activity_types))
 )
