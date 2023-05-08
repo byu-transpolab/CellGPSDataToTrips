944141cdea11b8d782648bd7322c6a6d4ef95c15
Emily Blanchard <ekblnchrd@gmail.com>
2023-05-08 14:40:46 -0600

    Update targets.R with Gillian's most recent stuff

--- _targets.R
+++ _targets.R
@@ -10,6 +10,7 @@ threads <- future::availableCores() - 1
 
 source("R/Optimize.R")
 source("R/MakeMaps.R")
+source("R/Mental Health Table.R")
 
 tar_option_set(debug = "cleaned_manual_table")
 
@@ -33,11 +34,11 @@ list(
   
   
   #tar_target(maps, makeAllMaps(cleaned_data)),
-  tar_target(optimized_sann_params, optimize(cleaned_manual_table)),
-  tar_target(optimized_optim_params, optimize2(cleaned_manual_table))
-  #tar_target(accurate_tibble, makeClusters(cleaned_manual_table = cleaned_data,
-  #params = optimized_params$par))
-  #tar_target(num_trips, addNumtrips(accurate_tibble)),
-  #tar_target(activity_types, addTtripType(num_trips)),
+  #tar_target(optimized_sann_params, optimize(cleaned_manual_table)),
+  #tar_target(optimized_optim_params, optimize2(cleaned_manual_table)),
+  tar_target(accurate_sann_tibble, makeClusters(cleaned_manual_table = cleaned_data,
+  params = c(11.7,3,300,1))),
+  tar_target(num_trips, addNumTrips(accurate_sann_tibble)),
+  tar_target(activity_types, addTripType(num_trips))
   #tar_target(final_table, addMentalHealthResponses(activity_types))
 )
